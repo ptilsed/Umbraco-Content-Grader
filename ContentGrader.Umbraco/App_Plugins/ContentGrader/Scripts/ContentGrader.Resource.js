d3a20de5a4b26146cf68315aeffd775aa992ed6b
paul <paul.tilsed@ph-creative.com>
2017-05-11 16:16:58 +0100

    Tweaks to the UI of the grader

---
+++ ContentGrader.Umbraco/App_Plugins/ContentGrader/Scripts/ContentGrader.Resource.js
@@ -0,0 +1,13 @@
+﻿angular.module("umbraco.resources")
+.factory("contentGraderResource", function ($http) {
+    return {
+        gradeContent: function (content) {
+            return $http({
+                method: 'POST',
+                url: 'backoffice/ContentGrader/ContentGrader/GradeContent',
+                data: JSON.stringify(content),
+                headers: { 'Content-Type': 'application/json' }
+            });
+        }
+    };
+});
---
+++ ContentGrader.Umbraco/App_Plugins/ContentGrader/Scripts/secondstominutes.filter.js
@@ -0,0 +1,19 @@
+﻿angular.module("umbraco.filters").filter('secondsToMinute', function () {
+
+    return function (timeInSeconds) {
+
+        if (isNaN(timeInSeconds)) {
+            return 'bad time. Enter time in seconds';
+        } else {
+            var minuteValue = parseInt(timeInSeconds / 60);
+            var secondsValue = timeInSeconds % 60;
+        }
+        if (minuteValue === 0)
+        {
+            return secondsValue + 'sec';
+        }
+        return minuteValue + 'min, ' + secondsValue + 'sec';
+
+    }
+
+});
