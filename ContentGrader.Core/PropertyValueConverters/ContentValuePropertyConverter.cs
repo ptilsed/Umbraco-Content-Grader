57ea62d6a87870d00a31a0a2b21c3ccf2125550c
paul <paul.tilsed@ph-creative.com>
2017-06-01 16:39:23 +0100

    Updating manifest

---
+++ ContentGrader.Core/PropertyValueConverters/ContentValuePropertyConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace ContentGrader.Core.PropertyValueConverters
+{
+    public class ContentValuePropertyConverter : IPropertyValueConverter
+    {
+        public object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source;
+        }
+
+        public object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return new HtmlString(source == null ? string.Empty : (string)source);
+        }
+
+        public object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source;
+        }
+
+        public bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.Equals("ContentGrader");
+        }
+    }
+}
